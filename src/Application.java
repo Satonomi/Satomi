dd5a3a32d41edbdfe2fe2fa87eb17fae3b11d26c
ninomichan <wamech@qq.com>
2018-04-18 22:21:19 +0800

    初始代码

---
+++ src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+	public void needBird(Bird bird){
+		int flyDistance=bird.fly();
+		System.out.println("这只鸟能飞行"+flyDistance+"米");
+	}
+	public static void main(String args[]){
+		Application client=new Application();
+		Bird sparrow=new Sparrow();
+		Bird sparrowDecorator1=new SparrowDecorator(sparrow);
+		Bird sparrowDecorator2=new SparrowDecorator(sparrowDecorator1);
+		client.needBird(sparrowDecorator1);
+		client.needBird(sparrowDecorator2);
+	}
+
+}
---
+++ src/Decorator.java
@@ -0,0 +1,10 @@
+public abstract class Decorator extends Bird{
+	
+	protected Bird bird;
+	public Decorator(){
+	}
+	public Decorator(Bird bird){
+		this.bird=bird;
+	}
+
+}
